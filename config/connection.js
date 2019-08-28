3689b8b0629a7c587b66db186293cbc10975c2ee
Mark Warness <markwarness@gmail.com>
2019-08-28 12:38:57 -0400

    added remote jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,57 @@
 // Set up MySQL connection.
-var mysql = require("mysql");
+// var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "IamRESPONSIBLE!3",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "IamRESPONSIBLE!3",
+//   database: "burgers_db"
+// });
 
 // Make connection.
-connection.connect(function(err) {
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+// Export connection for our ORM to use.
+// module.exports = connection;
+
+
+
+// --------------------------------------------------------------------
+// working code above this line for mysql db
+// --------------------------------------------------------------------
+// code for remote jawsDB
+
+
+// Set up MySQL connection.
+var mysql = require("mysql");
+var connection;
+
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "IamRESPONSIBLE!3",
+    database: "burgers_db"
+
+  })
+}
+
+
+
+// Make connection.
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
